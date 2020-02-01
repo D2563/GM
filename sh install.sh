2e6cf8103176053ef92e02692f9b21bcec6a8891
D2563 <60312300+D2563@users.noreply.github.com>
2020-02-01 07:05:08 +0700

    Create sh install.sh

---
+++ sh install.sh
@@ -0,0 +1,6 @@
+apt-get upgrade && apt-get update
+apt-get install git -y
+apt-get install python -y && apt-get install python2 -y
+apt-get install ไฮดรา -y
+apt-get install figlet -y
+apt-get dist-upgrade
